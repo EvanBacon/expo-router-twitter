b5678f0e64b030a346e5eb2282db8c12d6f5d9f0
Evan Bacon <baconbrix@gmail.com>
2023-05-02 01:39:23 -0500

    Update _layout.tsx

--- app/(index,search,profile)/_layout.tsx
+++ app/(index,search,profile)/_layout.tsx
@@ -1,11 +1,5 @@
 import { Stack } from "expo-router";
 
-export const unstable_settings = {
-  profile: {
-    initialRouteName: "profile",
-  },
-};
-
 export default function DynamicLayout({ segment }) {
   // Do dynamic settings using `segment`.
   return (
