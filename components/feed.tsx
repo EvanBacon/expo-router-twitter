e32ca1e6c0e8266cd69e2b107914e1f139b47c29
evanbacon <bacon@expo.io>
2023-05-06 16:02:39 -0500

    wip

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,19 +1,28 @@
-import { FlatList } from "react-native";
+import { FlatList, Platform } from "react-native";
 
 import { Post } from "../data";
 import { Tweet } from "./tweet";
-import { useScrollToTop } from "@react-navigation/native";
+import useScrollToTop from "./useScrollToTopWithOffset";
 import { useRef } from "react";
+import { useHeaderHeight } from "react-native-screens/native-stack";
 
 export function Feed({
   data,
   ...props
 }: { data: Post[] } & Partial<FlatList["props"]>) {
   const ref = useRef<FlatList>(null);
-  useScrollToTop(ref);
+  // const headerHeight = useHeaderHeight();
+  useScrollToTop(
+    ref,
+    Platform.select({
+      ios: -150,
+      default: 0,
+    })
+  );
   return (
     <FlatList
       ref={ref}
+      scrollToOverflowEnabled
       style={{ flex: 1 }}
       {...props}
       data={data}
